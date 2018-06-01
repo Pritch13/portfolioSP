30af1337d7afa1ce070cb8aadd86032469c895d4
Sam Pritchard <sam@Sams-MacBook-Pro.local>
2018-05-31 18:48:24 -0600

    scss install

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,64 +1,73 @@
 $('.port-main').hide();
 $('.skillSet').hide();
-$('#seeMore').hide();
+$('.main').hide();
 $('.carousel').carousel({
-    interval: 3000* 1,
+    interval: 3000 * 1,
     pause: false
 });
 
 window.sr = ScrollReveal();
-sr.reveal('#intro', {
-  duration: 2000,
-  origin: 'top'
-});
 
 sr.reveal('#portPicture', {
-  duration: 2000,
-  origin: 'top'
+    duration: 5000,
+    origin: 'top',
+    scale: 0.8
 });
 
 sr.reveal('#infoBtn', {
-    duration: 2000,
-    origin: 'bottom'
-  });
+    duration: 5000,
+    origin: 'bottom',
+    scale: 0.8
+});
 
+sr.reveal('.navbar', {
+    duration: 5000,
+    origin: 'bottom',
+    scale: 0.8
+});
+
+sr.reveal('footer', {
+    duration: 5000,
+    origin: 'top',
+    scale: 0.8
+});
 
 
 var typed = new Typed('#intro', {
     strings: ["Hi, i'm Sam."],
-    typeSpeed: 80,
+    typeSpeed: 160,
     backSpeed: 40
 });
 
-
-
 $('#infoBtn').on('click', function () {
 
-        $('.port-main').fadeIn(3500);
-        $('.picture-inc').fadeOut();
+    $('.port-main').fadeIn(3500);
+    $('.picture-inc').fadeOut();
     var typed = new Typed('#aboutMe', {
         strings: ["I’m a full stack web developer focused on producing professional and usable content.", "I develop websites that are both functional and visually engaging."],
         typeSpeed: 45,
         backSpeed: 10
     });
     $('.carousel').fadeIn(4000);
-    setTimeout(function(){
+    setTimeout(function () {
         $('.carousel').fadeOut();
-      }, 14000);
+        $('#seeMore').fadeOut(1000);
+        $('#aboutMe').fadeOut(1000);
+    }, 12000);
 
-      setTimeout(function(){
-        $('#seeMore').fadeIn();
-      }, 14400);
+    setInterval(function () {
+        $('.skillSet').fadeIn('slow');
+    }, 13000);
 
+    setInterval(function () {
+        $('.main').fadeIn();
+        $('.html').animate({ width: '95%' }, 2000);
+        $('.css').animate({ width: '85%' }, 2000);
+        $('.js').animate({ width: '72%' }, 2000);
+        $('.php').animate({ width: '70%' }, 2000);
+        $('.ajax').animate({ width: '48%' }, 2000);
+    }, 13600);
 });
 
-$('#seeMore').on('click', function(){
-    $('#seeMore').fadeOut(1000);
-    $('#aboutMe').fadeOut(1000);
 
 
-    setInterval(function(){
-    $('.skillSet').fadeIn('slow');
-      }, 1000);
-
-});
